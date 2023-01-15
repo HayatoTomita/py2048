1eb580181edca8c4b65e4ffca73d2406ef94b757
HayatoTomita <nice.to.tom@gmail.com>
2023-01-15 23:34:54 +0900

    packとmergeに上下の判定追加

--- py2048/module/board_array.py
+++ py2048/module/board_array.py
@@ -2,20 +2,23 @@ from typing import Literal
 
 import numpy as np
 
-
-Direction = Literal["LEFT", "RIGHT"]
+Direction = Literal["LEFT", "RIGHT", "TOP", "BOTTOM"]
 
 
 def pack(src: np.ndarray, pack_direction: Direction) -> np.ndarray:
     nonzero = np.nonzero(src)
     zero = np.where(src == 0)
-    tmp = (nonzero, zero) if pack_direction == "LEFT" else (zero, nonzero)
+    tmp = (
+        (nonzero, zero)
+        if pack_direction == "LEFT" or pack_direction == "TOP"
+        else (zero, nonzero)
+    )
     concatenated = np.concatenate(tmp, axis=1)[0]
     return src[concatenated]
 
 
 def merge(src: np.ndarray, merge_direction: Direction) -> np.ndarray:
-    if merge_direction == "RIGHT":
+    if merge_direction == "RIGHT" or merge_direction == "BOTTOM":
         src = src[::-1]
 
     for i in range(len(src) - 1):
@@ -23,7 +26,7 @@ def merge(src: np.ndarray, merge_direction: Direction) -> np.ndarray:
             src[i] = src[i] * 2
             src[i + 1] = 0
 
-    if merge_direction == "RIGHT":
+    if merge_direction == "RIGHT" or merge_direction == "BOTTOM":
         src = src[::-1]
 
     return src
