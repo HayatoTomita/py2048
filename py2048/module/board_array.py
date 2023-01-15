9ead569705ec6fd19cb36c661d6aee9f63811c07
HayatoTomita <nice.to.tom@gmail.com>
2023-01-15 23:35:57 +0900

    数字のスポーン、盤面初期化、packとmergeをまとめたコントロールを追加

--- py2048/module/board_array.py
+++ py2048/module/board_array.py
@@ -30,3 +30,31 @@ def merge(src: np.ndarray, merge_direction: Direction) -> np.ndarray:
         src = src[::-1]
 
     return src
+
+
+def get_spawn_idx(board: np.ndarray) -> np.ndarray:
+    rows, cols = np.where(board == 0)
+    idx = np.random.randint(0, len(rows))
+    return np.array([rows[idx], cols[idx]])
+
+
+def get_spawn_val() -> int:
+    return np.random.randint(1, 3) * 2
+
+
+def controll(board: np.ndarray, direction: Direction) -> np.ndarray:
+    axis = 0 if direction == "TOP" or direction == "BOTTOM" else 1
+    packed = np.apply_along_axis(pack, axis=axis, arr=board, pack_direction=direction)
+    merged = np.apply_along_axis(
+        merge, axis=axis, arr=packed, merge_direction=direction
+    )
+    result = np.apply_along_axis(pack, axis=axis, arr=merged, pack_direction=direction)
+    return result
+
+
+def init_board(board_size: int = 4) -> np.ndarray:
+    board = np.zeros(board_size * board_size)
+    board[0:2] = np.random.randint(1, 3) * 2
+    np.random.shuffle(board)
+    board = np.reshape(board, (board_size, board_size))
+    return board
