b27477463c2327431ce2ddeda43109c6a48a8142
HayatoTomita <nice.to.tom@gmail.com>
2023-01-19 00:00:05 +0900

    とりあえず仮で組んだ

---
+++ py2048/main.py
@@ -0,0 +1,50 @@
+import curses
+
+from module.board_array import (Direction, controll, get_spawn_idx,
+                                get_spawn_val, init_board, merge, pack)
+
+
+def main(stdscr):
+    curses.initscr()
+    board = init_board()
+
+    rows, cols = board.shape
+    total_score = 0
+    turn = 0
+    # Clear screen
+    while turn < 10:
+
+        stdscr.addstr(0, 0, "Turn : {}".format(turn))
+        stdscr.addstr(0, 10, "SCORE : {}".format(total_score))
+
+        for row in range(rows):
+            for col in range(cols):
+                stdscr.addstr(
+                    1 + 2 * row, col + 4 * (col + 1), "{}".format(int(board[row][col]))
+                )
+
+        c = stdscr.getch()
+
+        direction: Direction
+        if c == ord("w"):
+            direction = "TOP"
+        elif c == ord("a"):
+            direction = "LEFT"
+        elif c == ord("s"):
+            direction = "BOTTOM"
+        elif c == ord("d"):
+            direction = "RIGHT"
+
+        board, score, is_continue = controll(board, direction)
+        stdscr.addstr(0, 30, "is_continue : {}".format(is_continue))
+        if not is_continue:
+            continue
+
+        total_score += score
+        spawn_idx = get_spawn_idx(board)
+        board[tuple(spawn_idx)] = get_spawn_val()
+        turn += 1
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
