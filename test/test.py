f0ec8bc8ddf7ccb62f29d53d0567cfde2bfafb3e
HayatoTomita <nice.to.tom@gmail.com>
2023-01-18 23:57:04 +0900

    mergeのスコア機能追加に合わせてテストを修正

--- test/test.py
+++ test/test.py
@@ -16,35 +16,35 @@ def test_pack_rigth():
 
 def test_merge_left():
     input_array = np.array([2, 2, 0, 0])
-    result_array = np.array([4, 0, 0, 0])
+    result_array = np.array([4, 0, 0, 0, 2])
     assert all(board_array.merge(input_array, "LEFT") == result_array)
 
 
 def test_merge_rigth():
     input_array = np.array([0, 0, 2, 2])
-    result_array = np.array([0, 0, 0, 4])
+    result_array = np.array([0, 0, 0, 4, 2])
     assert all(board_array.merge(input_array, "RIGHT") == result_array)
 
 
 def test_merge_multi_three_left():
     input_array = np.array([2, 2, 2, 0])
-    result_array = np.array([4, 0, 2, 0])
+    result_array = np.array([4, 0, 2, 0, 2])
     assert all(board_array.merge(input_array, "LEFT") == result_array)
 
 
 def test_merge_multi_three_right():
     input_array = np.array([0, 2, 2, 2])
-    result_array = np.array([0, 2, 0, 4])
+    result_array = np.array([0, 2, 0, 4, 2])
     assert all(board_array.merge(input_array, "RIGHT") == result_array)
 
 
 def test_merge_multi_four_left():
     input_array = np.array([2, 2, 2, 2])
-    result_array = np.array([4, 0, 4, 0])
+    result_array = np.array([4, 0, 4, 0, 4])
     assert all(board_array.merge(input_array, "LEFT") == result_array)
 
 
 def test_merge_multi_four_right():
     input_array = np.array([2, 2, 2, 2])
-    result_array = np.array([0, 4, 0, 4])
+    result_array = np.array([0, 4, 0, 4, 4])
     assert all(board_array.merge(input_array, "RIGHT") == result_array)
